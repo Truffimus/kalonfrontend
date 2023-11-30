7c5f970b61ca251e05501c78f2b995338dd9ba81
Thomas Musyimi <46557207+Tom-Hunter@users.noreply.github.com>
2023-11-30 22:40:35 +0300

    FIxed routing on navbar and added generic logos.

--- kalon/src/app/app.module.ts
+++ kalon/src/app/app.module.ts
@@ -5,9 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { ViewbookComponent } from './pages/viewbook/viewbook.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AddbookComponent } from './pages/addbook/addbook.component';
@@ -22,17 +22,17 @@ import { HttpClientModule } from '@angular/common/http';
     ViewbookComponent,
     SidebarComponent,
     HomepageComponent,
-    BookComponent
+    BookComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NavbarComponent,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
